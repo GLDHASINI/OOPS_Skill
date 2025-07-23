f66e2e45b520319ca6247306bbf9e654e11842a5
GLDHASINI <2420030421@klh.edu.in>
2025-07-23 11:35:13 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class TypeCasting {
+public static void main(String args[]) {
+	double d =10.75;
+	int value =0;
+	int n=(int) value;
+	System.out.println("Double value: " + d);
+	System.out.println("Cast to int: " + n);
+}
+}
+	
